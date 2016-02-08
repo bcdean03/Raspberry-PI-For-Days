c86d4814e2e866fc5580916d6b6130a6e6d7c327
luludoobee <bcdean03@gmail.com>
2016-02-08 16:33:51 -0700

    Working all around. Need to run "sudo hciconfig hci0 piscan" for raspberry pi to allow it to search. Going to have to run it through python or the terminal to automatically run when booting up raspberry pi

--- ClientTest1.py
+++ ClientTest1.py
@@ -2,13 +2,18 @@
 
 import bluetooth
 
-bd_addr = "FC:F8:AE:36:F4:42"
-
+#bd_addr = "FC:F8:AE:36:F4:42"
+bd_addr = "5C:F3:70:71:A0:B5"
 port = 1
 
 sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect((bd_addr, port))
 
-sock.send("hello!!")
+sock.send("Abel Dean Josh Ben John Shelley Jalen"
+          "Abel Dean Josh Ben John Shelley Jalen Abel Dean "
+          "Josh Ben John Shelley Jalen Abel Dean"
+          "Josh Ben John Shelley Jalen Abel Abel"
+          "Dean Josh Ben John Shelley Jalen")
+
 
 sock.close()
